1030e6493c287259dc3f195263ba333a428003df
sejaljadhav21 <sejal.j0021@gmail.com>
2024-08-01 12:02:11 +0530

    Initial commit

--- collectdata.py
+++ collectdata.py
@@ -2,40 +2,40 @@
 import os
 import cv2
 cap=cv2.VideoCapture(0)
-directory='Image/'
+directory='D:/PBL/SignLanguageDetectionUsingML/Image/'
 while True:
     _,frame=cap.read()
     count = {
              'a': len(os.listdir(directory+"/A")),
              'b': len(os.listdir(directory+"/B")),
              'c': len(os.listdir(directory+"/C")),
-             'd': len(os.listdir(directory+"/D")),
-             'e': len(os.listdir(directory+"/E")),
-             'f': len(os.listdir(directory+"/F")),
-             'g': len(os.listdir(directory+"/G")),
-             'h': len(os.listdir(directory+"/H")),
-             'i': len(os.listdir(directory+"/I")),
-             'j': len(os.listdir(directory+"/J")),
-             'k': len(os.listdir(directory+"/K")),
-             'l': len(os.listdir(directory+"/L")),
-             'm': len(os.listdir(directory+"/M")),
-             'n': len(os.listdir(directory+"/N")),
-             'o': len(os.listdir(directory+"/O")),
-             'p': len(os.listdir(directory+"/P")),
-             'q': len(os.listdir(directory+"/Q")),
-             'r': len(os.listdir(directory+"/R")),
-             's': len(os.listdir(directory+"/S")),
-             't': len(os.listdir(directory+"/T")),
-             'u': len(os.listdir(directory+"/U")),
-             'v': len(os.listdir(directory+"/V")),
-             'w': len(os.listdir(directory+"/W")),
-             'x': len(os.listdir(directory+"/X")),
-             'y': len(os.listdir(directory+"/Y")),
-             'z': len(os.listdir(directory+"/Z"))
+            #  'd': len(os.listdir(directory+"/D")),
+            #  'e': len(os.listdir(directory+"/E")),
+            #  'f': len(os.listdir(directory+"/F")),
+            #  'g': len(os.listdir(directory+"/G")),
+            #  'h': len(os.listdir(directory+"/H")),
+            #  'i': len(os.listdir(directory+"/I")),
+            #  'j': len(os.listdir(directory+"/J")),
+            #  'k': len(os.listdir(directory+"/K")),
+            #  'l': len(os.listdir(directory+"/L")),
+            #  'm': len(os.listdir(directory+"/M")),
+            #  'n': len(os.listdir(directory+"/N")),
+            #  'o': len(os.listdir(directory+"/O")),
+            #  'p': len(os.listdir(directory+"/P")),
+            #  'q': len(os.listdir(directory+"/Q")),
+            #  'r': len(os.listdir(directory+"/R")),
+            #  's': len(os.listdir(directory+"/S")),
+            #  't': len(os.listdir(directory+"/T")),
+            #  'u': len(os.listdir(directory+"/U")),
+            #  'v': len(os.listdir(directory+"/V")),
+            #  'w': len(os.listdir(directory+"/W")),
+            #  'x': len(os.listdir(directory+"/X")),
+            #  'y': len(os.listdir(directory+"/Y")),
+            #  'z': len(os.listdir(directory+"/Z"))
              }
-    # cv2.putText(frame, "a : "+str(count['a']), (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    # cv2.putText(frame, "b : "+str(count['b']), (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
-    # cv2.putText(frame, "c : "+str(count['c']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "a : "+str(count['a']), (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "b : "+str(count['b']), (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
+    cv2.putText(frame, "c : "+str(count['c']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     # cv2.putText(frame, "d : "+str(count['d']), (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     # cv2.putText(frame, "e : "+str(count['e']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
     # cv2.putText(frame, "f : "+str(count['f']), (10, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)
